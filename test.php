1ad2c75c8df10e858f7b9993c6a08fab78e28e6e
c0d1007 <43197505+c0d1007@users.noreply.github.com>
2019-04-17 17:42:13 +0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: M4xR0
+ * Date: 2019/4/17
+ * Time: 9:36
+ */
+
+$var = $_GET['test'];
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+    <title>Server</title>
+</head>
+
+
+<body>
+<?php echo $var; ?>
+</body>
+
+
+</html>
+
+
